4a520982a209992f437e1295b05752d616799c92
Heylium <tuanhsiang@163.com>
2022-09-02 22:40:01 +0800

    lib file added

---
+++ actix-backend/frontend/src/lib.rs
@@ -0,0 +1,26 @@
+// Fix for now: https://github.com/rustwasm/wasm-bindgen/issues/2774
+#![allow(clippy::unused_unit)]
+
+pub mod app;
+pub mod components;
+pub mod error;
+pub mod hooks;
+pub mod routes;
+pub mod services;
+pub mod types;
+
+use wasm_bindgen::prelude::*;
+
+use app::App;
+
+// Use `wee_alloc` as the global allocator.
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+// This is the entry point for the web app
+#[wasm_bindgen]
+pub fn run_app() -> Result<(), JsValue> {
+    wasm_logger::init(wasm_logger::Config::default());
+    yew::start_app::<App>();
+    Ok(())
+}
