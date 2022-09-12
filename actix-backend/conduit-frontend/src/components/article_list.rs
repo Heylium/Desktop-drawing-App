c14be4f421dda23b83ded6fe6d22063f3c716543
Heylium <tuanhsiang@163.com>
2022-09-12 23:14:51 +0800

    move old frontend folder to conduit-frontend folder

---
+++ actix-backend/conduit-frontend/src/components/article_list.rs
@@ -0,0 +1,95 @@
+use yew::prelude::*;
+use yew_hooks::prelude::*;
+
+use super::article_preview::ArticlePreview;
+use super::list_pagination::ListPagination;
+use crate::services::articles::*;
+
+#[derive(Properties, Clone, PartialEq)]
+pub struct Props {
+    pub filter: ArticleListFilter,
+}
+
+/// Filters for article list
+#[derive(Clone, Debug, PartialEq)]
+pub enum ArticleListFilter {
+    All,
+    ByAuthor(String),
+    ByTag(String),
+    FavoritedBy(String),
+    Feed,
+}
+
+/// List of articles component
+#[function_component(ArticleList)]
+pub fn article_list(props: &Props) -> Html {
+    let current_page = use_state(|| 0u32);
+    let article_list = {
+        let filter = props.filter.clone();
+        let current_page = current_page.clone();
+
+        use_async(async move {
+            match filter {
+                ArticleListFilter::All => all(*current_page).await,
+                ArticleListFilter::ByAuthor(author) => by_author(author, *current_page).await,
+                ArticleListFilter::ByTag(tag) => by_tag(tag, *current_page).await,
+                ArticleListFilter::FavoritedBy(author) => favorited_by(author, *current_page).await,
+                ArticleListFilter::Feed => feed().await,
+            }
+        })
+    };
+
+    {
+        let current_page = current_page.clone();
+        use_effect_with_deps(
+            move |_| {
+                // Reset to first page
+                current_page.set(0);
+                || ()
+            },
+            props.filter.clone(),
+        );
+    }
+
+    {
+        let article_list = article_list.clone();
+        use_effect_with_deps(
+            move |_| {
+                article_list.run();
+                || ()
+            },
+            (props.filter.clone(), *current_page),
+        );
+    }
+
+    let callback = {
+        let current_page = current_page.clone();
+        Callback::from(move |page| {
+            current_page.set(page);
+        })
+    };
+
+    if let Some(article_list) = &article_list.data {
+        if !article_list.articles.is_empty() {
+            html! {
+                <>
+                    {for article_list.articles.iter().map(|article| {
+                        html! { <ArticlePreview article={article.clone()} /> }
+                    })}
+                    <ListPagination
+                        total_count={article_list.articles_count}
+                        current_page={*current_page}
+                        callback={callback} />
+                </>
+            }
+        } else {
+            html! {
+                <div class="article-preview">{ "No articles are here... yet." }</div>
+            }
+        }
+    } else {
+        html! {
+            <div class="article-preview">{ "Loading..." }</div>
+        }
+    }
+}
---
+++ actix-backend/conduit-frontend/src/components/article_preview.rs
@@ -0,0 +1,104 @@
+use yew::prelude::*;
+use yew_hooks::prelude::*;
+use yew_router::prelude::*;
+
+use crate::routes::AppRoute;
+use crate::services::articles::*;
+use crate::types::ArticleInfo;
+
+const FAVORITED_CLASS: &str = "btn btn-sm btn-primary";
+const NOT_FAVORITED_CLASS: &str = "btn btn-sm btn-outline-primary";
+
+#[derive(Properties, Clone, PartialEq)]
+pub struct Props {
+    pub article: ArticleInfo,
+}
+
+/// Single article preview component used by article list.
+#[function_component(ArticlePreview)]
+pub fn article_preview(props: &Props) -> Html {
+    let article = use_state(|| props.article.clone());
+    let article_favorite = {
+        let article = article.clone();
+        use_async(async move {
+            if article.favorited {
+                unfavorite(article.slug.clone()).await
+            } else {
+                favorite(article.slug.clone()).await
+            }
+        })
+    };
+
+    {
+        let article = article.clone();
+        let article_favorite = article_favorite.clone();
+        use_effect_with_deps(
+            move |article_favorite| {
+                if let Some(article_info) = &article_favorite.data {
+                    article.set(article_info.article.clone());
+                }
+                || ()
+            },
+            article_favorite,
+        );
+    }
+
+    let favorite_button_class = if article.favorited {
+        FAVORITED_CLASS
+    } else {
+        NOT_FAVORITED_CLASS
+    };
+    let onclick = {
+        Callback::from(move |ev: MouseEvent| {
+            ev.prevent_default();
+            article_favorite.run();
+        })
+    };
+
+    html! {
+        <div class="article-preview">
+            <div class="article-meta">
+                <img src={article.author.image.clone()} />
+                <div class="info">
+                    <Link<AppRoute>
+                        to={AppRoute::Profile { username: article.author.username.clone() }}
+                        classes="author" >
+                        { &article.author.username }
+                    </Link<AppRoute>>
+                    <span class="date">
+                        { &article.created_at.format("%B %e, %Y") }
+                    </span>
+                </div>
+                <div class="pull-xs-right">
+                    <button class={favorite_button_class} onclick={onclick}>
+                        <i class="ion-heart"></i> { article.favorites_count }
+                    </button>
+                </div>
+            </div>
+            <h1>
+                <Link<AppRoute>
+                    to={AppRoute::Article { slug: article.slug.clone() }}
+                    classes="preview-link" >
+                { &article.title }
+                </Link<AppRoute>>
+            </h1>
+            <p>{ &article.description }</p>
+            <span>
+                <Link<AppRoute>
+                    to={AppRoute::Article { slug: article.slug.clone() }}
+                    classes="preview-link" >
+                    { "Read more..." }
+                </Link<AppRoute>>
+            </span>
+            <ul class="tag-list">
+                {for article.tag_list.iter().map(|tag| {
+                    html! {
+                        <li class="tag-default tag-pill tag-outline" key={ (&tag).to_string() }>
+                            { &tag }
+                        </li>
+                    }
+                })}
+            </ul>
+        </div>
+    }
+}
