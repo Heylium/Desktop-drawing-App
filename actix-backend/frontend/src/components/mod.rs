0f71ee3d3fb58bdee23d8212310bdaf69e1db9a2
Heylium <tuanhsiang@163.com>
2022-09-12 23:12:16 +0800

    redunt files removed

--- actix-backend/frontend/src/components/mod.rs
+++
@@ -1,9 +0,0 @@
-//! Components used by route pages
-
-pub mod article_list;
-pub mod article_preview;
-pub mod footer;
-pub mod header;
-pub mod list_errors;
-pub mod list_pagination;
-pub mod user_context_provider;
