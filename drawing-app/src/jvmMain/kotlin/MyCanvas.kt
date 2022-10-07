2b3126e241664e6176f033ce1d27d139f86fb611
Heylium <tuanhsiang@163.com>
2022-10-07 22:21:20 +0800

    added a custom canvas

---
+++ drawing-app/src/jvmMain/kotlin/MyCanvas.kt
@@ -0,0 +1,20 @@
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+
+@Composable
+fun MyCanvas() {
+
+    Canvas(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White)
+    ) {
+
+    }
+}
