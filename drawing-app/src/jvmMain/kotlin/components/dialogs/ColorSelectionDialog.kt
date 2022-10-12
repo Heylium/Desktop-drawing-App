5d3a8dc460125527eed7182defed6e7e38ba6312
Heylium <tuanhsiang@163.com>
2022-10-12 22:25:28 +0800

    Update ColorSelectionDialog.kt

--- drawing-app/src/jvmMain/kotlin/components/dialogs/ColorSelectionDialog.kt
+++ drawing-app/src/jvmMain/kotlin/components/dialogs/ColorSelectionDialog.kt
@@ -22,7 +22,6 @@ import ui.ColorWheel
 import ui.theme.Blue400
 import kotlin.math.roundToInt
 
-@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun ColorSelectionDialog(
     initialColor: Color,
@@ -42,180 +41,147 @@ fun ColorSelectionDialog(
         alpha = alpha.roundToInt()
     )
 
-//    AlertDialog(
-//        onDismissRequest = onDismiss,
-//        title = { Text(
-//            text = "Color",
-//            color = Blue400,
-//            fontSize = 18.sp,
-//            fontWeight = FontWeight.Bold,
-//            modifier = Modifier.padding(top = 12.dp)
-//        ) },
-//        confirmButton = {
-//            TextButton(
-//                modifier = Modifier
-//                    .weight(1f)
-//                    .fillMaxHeight(),
-//                onClick = {
-//                    onPositiveClick(color)
-//                },
-//            ) {
-//                Text(text = "OK")
-//            }
-//        },
-//        dismissButton = {
-//            TextButton(
-//                onClick = onNegativeClick,
-//                modifier = Modifier
-//                    .weight(1f)
-//                    .fillMaxHeight()
-//            ) {
-//                Text(text = "CANCEL")
-//            }
-//        },
-//
-//    )
-
     var dialogState = remember { mutableStateOf(false) }
     Dialog(
         onCloseRequest = onDismiss,
-    ) {
-        BoxWithConstraints(
-            Modifier.shadow(1.dp, RoundedCornerShape(8.dp))
-                .background(Color.White)
-        ) {
-            val widthInDp = LocalDensity.current.run { maxWidth }
+        content = {
+            BoxWithConstraints(
+                Modifier.shadow(1.dp, RoundedCornerShape(8.dp))
+                    .background(Color.White)
+            ) {
+                val widthInDp = LocalDensity.current.run { maxWidth }
 
-            Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                Text(
-                    text = "Color",
-                    color = Blue400,
-                    fontSize = 18.sp,
-                    fontWeight = FontWeight.Bold,
-                    modifier = Modifier.padding(top = 12.dp)
-                )
-
-                // Initial and Current Colors
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 50.dp, vertical = 20.dp)
-                ) {
-
-                    Box(
-                        modifier = Modifier
-                            .weight(1f)
-                            .height(40.dp)
-                            .background(
-                                initialColor,
-                                shape = RoundedCornerShape(topStart = 8.dp, bottomStart = 8.dp)
-                            )
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Text(
+                        text = "Color",
+                        color = Blue400,
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.padding(top = 12.dp)
                     )
-                    Box(
+
+                    // Initial and Current Colors
+                    Row(
                         modifier = Modifier
-                            .weight(1f)
-                            .height(40.dp)
-                            .background(
-                                color,
-                                shape = RoundedCornerShape(topEnd = 8.dp, bottomEnd = 8.dp)
-                            )
+                            .fillMaxWidth()
+                            .padding(horizontal = 50.dp, vertical = 20.dp)
+                    ) {
+
+                        Box(
+                            modifier = Modifier
+                                .weight(1f)
+                                .height(40.dp)
+                                .background(
+                                    initialColor,
+                                    shape = RoundedCornerShape(topStart = 8.dp, bottomStart = 8.dp)
+                                )
+                        )
+                        Box(
+                            modifier = Modifier
+                                .weight(1f)
+                                .height(40.dp)
+                                .background(
+                                    color,
+                                    shape = RoundedCornerShape(topEnd = 8.dp, bottomEnd = 8.dp)
+                                )
+                        )
+                    }
+
+                    ColorWheel(
+                        modifier = Modifier
+                            .width(widthInDp * .8f)
+                            .aspectRatio(1f)
                     )
-                }
 
-                ColorWheel(
-                    modifier = Modifier
-                        .width(widthInDp * .8f)
-                        .aspectRatio(1f)
-                )
+                    Spacer(modifier = Modifier.height(16.dp))
 
-                Spacer(modifier = Modifier.height(16.dp))
-
-                // Sliders
-                ColorSlider(
-                    modifier = Modifier
-                        .padding(start = 12.dp, end = 12.dp)
-                        .fillMaxWidth(),
-                    title = "Red",
-                    titleColor = Color.Red,
-                    rgb = red,
-                    onColorChanged = {
-                        red = it
-                    }
-                )
-
-                Spacer(modifier = Modifier.height(4.dp))
-
-                ColorSlider(
-                    modifier = Modifier
-                        .padding(start = 12.dp, end = 12.dp)
-                        .fillMaxWidth(),
-                    title = "Green",
-                    titleColor = Color.Green,
-                    rgb = green,
-                    onColorChanged = {
-                        green = it
-                    }
-                )
-
-                Spacer(modifier = Modifier.height(4.dp))
-
-                ColorSlider(
-                    modifier = Modifier
-                        .padding(start = 12.dp, end = 12.dp)
-                        .fillMaxWidth(),
-                    title = "Blue",
-                    titleColor = Color.Blue,
-                    rgb = blue,
-                    onColorChanged = {
-                        blue = it
-                    }
-                )
-
-                Spacer(modifier = Modifier.height(4.dp))
-
-                ColorSlider(
-                    modifier = Modifier
-                        .padding(start = 12.dp, end = 12.dp)
-                        .fillMaxWidth(),
-                    title = "Alpha",
-                    titleColor = Color.Black,
-                    rgb = alpha,
-                    onColorChanged = {
-                        alpha = it
-                    }
-                )
-                Spacer(modifier = Modifier.height(24.dp))
-
-                // Buttons
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(60.dp)
-                        .background(Color(0xffF3E5F5)),
-                    verticalAlignment = Alignment.CenterVertically
-
-                ) {
-
-                    TextButton(
-                        onClick = onNegativeClick,
+                    // Sliders
+                    ColorSlider(
                         modifier = Modifier
-                            .weight(1f)
-                            .fillMaxHeight()
-                    ) {
-                        Text(text = "CANCEL")
-                    }
-                    TextButton(
+                            .padding(start = 12.dp, end = 12.dp)
+                            .fillMaxWidth(),
+                        title = "Red",
+                        titleColor = Color.Red,
+                        rgb = red,
+                        onColorChanged = {
+                            red = it
+                        }
+                    )
+
+                    Spacer(modifier = Modifier.height(4.dp))
+
+                    ColorSlider(
                         modifier = Modifier
-                            .weight(1f)
-                            .fillMaxHeight(),
-                        onClick = {
-                            onPositiveClick(color)
-                        },
+                            .padding(start = 12.dp, end = 12.dp)
+                            .fillMaxWidth(),
+                        title = "Green",
+                        titleColor = Color.Green,
+                        rgb = green,
+                        onColorChanged = {
+                            green = it
+                        }
+                    )
+
+                    Spacer(modifier = Modifier.height(4.dp))
+
+                    ColorSlider(
+                        modifier = Modifier
+                            .padding(start = 12.dp, end = 12.dp)
+                            .fillMaxWidth(),
+                        title = "Blue",
+                        titleColor = Color.Blue,
+                        rgb = blue,
+                        onColorChanged = {
+                            blue = it
+                        }
+                    )
+
+                    Spacer(modifier = Modifier.height(4.dp))
+
+                    ColorSlider(
+                        modifier = Modifier
+                            .padding(start = 12.dp, end = 12.dp)
+                            .fillMaxWidth(),
+                        title = "Alpha",
+                        titleColor = Color.Black,
+                        rgb = alpha,
+                        onColorChanged = {
+                            alpha = it
+                        }
+                    )
+                    Spacer(modifier = Modifier.height(24.dp))
+
+                    // Buttons
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(60.dp)
+                            .background(Color(0xffF3E5F5)),
+                        verticalAlignment = Alignment.CenterVertically
+
                     ) {
-                        Text(text = "OK")
+
+                        TextButton(
+                            onClick = onNegativeClick,
+                            modifier = Modifier
+                                .weight(1f)
+                                .fillMaxHeight()
+                        ) {
+                            Text(text = "CANCEL")
+                        }
+                        TextButton(
+                            modifier = Modifier
+                                .weight(1f)
+                                .fillMaxHeight(),
+                            onClick = {
+                                onPositiveClick(color)
+                            },
+                        ) {
+                            Text(text = "OK")
+                        }
                     }
                 }
             }
         }
-    }
+    )
 }
