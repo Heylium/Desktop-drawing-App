b2d5f8ff09e0d91586cb719bd6ca1bcccbdc1bfe
Heylium <tuanhsiang@163.com>
2022-10-17 22:45:33 +0800

    Update CustomCanvas.kt

--- drawing-app/src/jvmMain/kotlin/customCanvas/CustomCanvas.kt
+++ drawing-app/src/jvmMain/kotlin/customCanvas/CustomCanvas.kt
@@ -1,25 +1,39 @@
-package customCanvas.moves
+package customCanvas
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.forEachGesture
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.PointerInputChange
-import customCanvas.moves.awaitClickEvent
+import androidx.compose.ui.input.pointer.pointerInput
+
 
 @Composable
 fun CustomCanvas(){
+    @OptIn(ExperimentalFoundationApi::class)
+
+    var motionEvent by remember { mutableStateOf(false) }
+    var currentPosition by remember { mutableStateOf(Offset.Unspecified) }
+
     Canvas(
         modifier = Modifier
             .fillMaxSize()
             .background(color = Color.Gray)
-            .clickMotionEvent(
-                onClick = { pointerInputChange: PointerInputChange ->
-
+            .pointerInput(Unit) {
+                forEachGesture {
+                    awaitPointerEventScope {
+                        awaitFirstDown().also {
+                            motionEvent = true
+                            currentPosition = it.position
+                        }
+                    }
                 }
-            )
+            }
     ){}
 
 }
