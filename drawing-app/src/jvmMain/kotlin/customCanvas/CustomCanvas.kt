3d52d441a6c887dc94953ee0f9dde7fa4b1cd314
Heylium <tuanhsiang@163.com>
2022-10-18 23:10:37 +0800

    Update CustomCanvas.kt

--- drawing-app/src/jvmMain/kotlin/customCanvas/CustomCanvas.kt
+++ drawing-app/src/jvmMain/kotlin/customCanvas/CustomCanvas.kt
@@ -9,16 +9,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.graphics.Path
+
 
 
 @Composable
 fun CustomCanvas(){
-    @OptIn(ExperimentalFoundationApi::class)
 
     var motionEvent by remember { mutableStateOf(false) }
     var currentPosition by remember { mutableStateOf(Offset.Unspecified) }
+    val paths = remember { mutableStateListOf<Path>() }
 
     Canvas(
         modifier = Modifier
@@ -34,6 +37,21 @@ fun CustomCanvas(){
                     }
                 }
             }
-    ){}
+    ){
+        with(drawContext.canvas.nativeCanvas) {
+            val checkPoint = saveLayer(null, null)
+            paths.forEach { it: Path ->
+                drawPath(
+                    color = Color.Black,
+                    path = it,
+                    style = Stroke(
+                        width = 4f,
+                        cap = StrokeCap.Round,
+                        join = StrokeJoin.Round,
+                    )
+                )
+            }
+        }
+    }
 
 }
