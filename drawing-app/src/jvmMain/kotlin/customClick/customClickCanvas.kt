53a66a5fd61f05972fc96604eb890bd6162ac596
Heylium <tuanhsiang@163.com>
2022-11-08 22:21:40 +0800

    mouse pointer move event add to custom canvas

--- drawing-app/src/jvmMain/kotlin/customClick/customClickCanvas.kt
+++ drawing-app/src/jvmMain/kotlin/customClick/customClickCanvas.kt
@@ -1,10 +1,31 @@
 package customClick
 
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.PointerEventType
+import androidx.compose.ui.input.pointer.onPointerEvent
 
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun clickCanvas() {
+
+
+    Canvas(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Gray)
+            .onPointerEvent(PointerEventType.Move) {
+                val position = it.changes.first().position
+            }
+    ) {
+
+    }
 
 
 
