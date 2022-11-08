133e7d5061e57953090ada29d8ff33126d727986
Heylium <tuanhsiang@163.com>
2022-11-09 00:01:44 +0800

    test mouse event code added

--- drawing-app/src/jvmMain/kotlin/customClick/customClickCanvas.kt
+++ drawing-app/src/jvmMain/kotlin/customClick/customClickCanvas.kt
@@ -1,29 +1,45 @@
 package customClick
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.onClick
+import androidx.compose.runtime.*
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.PointerEventType
 import androidx.compose.ui.input.pointer.onPointerEvent
+import androidx.compose.ui.input.pointer.pointerInput
 
 
-@OptIn(ExperimentalComposeUiApi::class)
+@OptIn(ExperimentalComposeUiApi::class, ExperimentalFoundationApi::class)
 @Composable
 fun clickCanvas() {
+    var color by remember { mutableStateOf(Color(0, 0, 0)) }
 
 
     Canvas(
         modifier = Modifier
             .fillMaxSize()
-            .background(color = Color.Gray)
+            .background(color = color)
             .onPointerEvent(PointerEventType.Move) {
                 val position = it.changes.first().position
+                color = Color(position.x.toInt() % 256, position.y.toInt() %256, 0)
+            }
+            .onPointerEvent(PointerEventType.Press) {
+                val presPosi = it.changes.first().position
+                println("pressed: $presPosi")
+            }
+            .pointerInput(Unit) {
+                detectDragGestures { change, dragAmount ->
+                    println("drag change: ${change.position}")
+                }
             }
     ) {
+
 
     }
 
