e15966e59b7219d5db95a1ccae3a4bd19fb08848
Heylium <tuanhsiang@163.com>
2022-11-11 23:59:48 +0800

    point color list added

--- drawing-app/src/jvmMain/kotlin/customClick/customClickCanvas.kt
+++ drawing-app/src/jvmMain/kotlin/customClick/customClickCanvas.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.*
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.input.pointer.PointerEvent
@@ -16,7 +17,7 @@ import androidx.compose.ui.input.pointer.PointerEventType
 import androidx.compose.ui.input.pointer.onPointerEvent
 import androidx.compose.ui.input.pointer.pointerInput
 
-data class Point(val x: Float, val y: Float)
+data class Point(val x: Float, val y: Float, var color: Color = Color.Black)
 data class PathProperties(
     val startPoint: Point,
     val endPoint: Point,
@@ -27,33 +28,41 @@ data class PathProperties(
 fun clickCanvas() {
     val pointList = remember { mutableStateListOf<Point>() }
     val pathList = remember { mutableStateListOf<Path>() }
+    val rectList = remember { mutableStateListOf<Rect>() }
+    val colorList = remember { mutableStateListOf<Color>() }
     val path by remember { mutableStateOf(Path()) }
-
 
 
     Canvas(
         modifier = Modifier
             .fillMaxSize()
             .background(color = Color.Gray)
-            .onPointerEvent(PointerEventType.Move) {movePointerEvent: PointerEvent ->
-                val position = movePointerEvent.changes.first().position
-//                color = Color(position.x.toInt() % 256, position.y.toInt() %256, 0)
-            }
             .onPointerEvent(PointerEventType.Press) {pressPointerEvent: PointerEvent ->
                 val currPosition = pressPointerEvent.changes.first().position
 
-
-
-                if (pointList.isEmpty()) {
-                    pointList.add(Point(currPosition.x, currPosition.y))
-                } else {
+                if (pointList.isNotEmpty()) {
                     val prevPosition = pointList.last()
-                    pointList.add(Point(currPosition.x, currPosition.y))
                     path.moveTo(prevPosition.x, prevPosition.y)
                     path.lineTo(currPosition.x, currPosition.y)
                     pathList.add(path)
                 }
+                pointList.add(Point(currPosition.x, currPosition.y, Color.Black))
+                rectList.add(Rect(left = currPosition.x - 6f, right = currPosition.x + 6f, top = currPosition.y - 6f, bottom = currPosition.y + 6f))
+                colorList.add(Color.Black)
 
+            }
+            .onPointerEvent(PointerEventType.Move) {movePointerEvent: PointerEvent ->
+                val position = movePointerEvent.changes.first().position
+//                color = Color(position.x.toInt() % 256, position.y.toInt() %256, 0)
+                for (idx in rectList.indices.reversed()) {
+                    if (rectList[idx].contains(position)) {
+//                        pointList[idx].color = Color.Red
+                        colorList[idx] = Color.Red
+                    } else {
+//                        pointList[idx].color = Color.Black
+                        colorList[idx] = Color.Black
+                    }
+                }
             }
             .pointerInput(Unit) {
                 detectDragGestures { change, dragAmount ->
@@ -73,11 +82,11 @@ fun clickCanvas() {
                 )
             )
         }
-        pointList.forEach { point: Point ->
+        pointList.forEachIndexed() { idx, point: Point ->
             drawCircle(
-                color = Color.Black,
+                color = colorList[idx],
                 radius = 6f,
-                center = Offset(point.x, point.y)
+                center = Offset(point.x, point.y),
             )
         }
 
