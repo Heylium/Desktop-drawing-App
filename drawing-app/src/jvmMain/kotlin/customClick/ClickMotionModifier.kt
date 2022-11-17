d00162c5a5ad89affb5951e8fbeed555fc493ffc
Heylium <tuanhsiang@163.com>
2022-11-18 00:17:05 +0800

    Update ClickMotionModifier.kt

--- drawing-app/src/jvmMain/kotlin/customClick/ClickMotionModifier.kt
+++ drawing-app/src/jvmMain/kotlin/customClick/ClickMotionModifier.kt
@@ -2,29 +2,28 @@ package customClick
 
 import androidx.compose.foundation.gestures.awaitFirstDown
 import androidx.compose.foundation.gestures.forEachGesture
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.pointer.*
+import androidx.compose.ui.input.pointer.AwaitPointerEventScope
+import androidx.compose.ui.input.pointer.pointerInput
 
+enum class MouseEvent {
+    Down, Move, Up
+}
 
-@OptIn(ExperimentalComposeUiApi::class)
-fun Modifier.clickMotionEvent(
-    onPointerMove: (PointerEventType) -> Unit = {},
+fun Modifier.mouseMotionEvent (
 
-) = this.then(
+) =this.then(
     Modifier.pointerInput(Unit) {
         forEachGesture {
             awaitPointerEventScope {
-                onPointerEvent(PointerEventType.Move) {pointerEvent: PointerEvent ->
-                    val position = pointerEvent.changes.first().position
-                }
+                awaitMouseMotionEvent()
             }
         }
     }
 )
 
-
-suspend fun AwaitPointerEventScope.awaitMouseEvent(
-) {
+suspend fun AwaitPointerEventScope.awaitMouseMotionEvent() {
+    val p = awaitFirstDown()
+//    println("pointer at ${p.position}")
 
 }
