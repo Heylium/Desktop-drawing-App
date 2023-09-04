e94cb698bd2108741968e4361d414ecd883c3f35
Heylium <tuanhsiang@163.com>
2023-09-04 23:37:25 +0800

    Create Main.kt

---
+++ async_draw_app/src/main/kotlin/Main.kt
@@ -0,0 +1,26 @@
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+import drawbox.canvas.DrawBoxAsync
+
+@Composable
+@Preview
+fun App() {
+    var text by remember { mutableStateOf("Hello, World!") }
+
+    MaterialTheme {
+        DrawBoxAsync()
+    }
+}
+
+fun main() = application {
+    Window(onCloseRequest = ::exitApplication) {
+        App()
+    }
+}
