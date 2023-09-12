b906c7c8e898cc691beb741100a89d3cb22c4d92
Heylium <tuanhsiang@163.com>
2023-09-12 23:27:47 +0800

    Update DrawController2.kt
    Custom draw controller fields added.

--- async_draw_app/src/main/kotlin/drawbox/canvas/controller/DrawController2.kt
+++ async_draw_app/src/main/kotlin/drawbox/canvas/controller/DrawController2.kt
@@ -2,8 +2,10 @@ package drawbox.canvas.controller
 
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Canvas
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.Path
+import io.github.markyav.drawbox.controller.DrawBoxBackground
 import io.github.markyav.drawbox.controller.DrawBoxConnectionState
 import io.github.markyav.drawbox.model.PathWrapper
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -22,4 +24,18 @@ class DrawController2 {
 
     val openedImage: MutableStateFlow<ImageBitmap?> = MutableStateFlow(null)
 
+    /** An [canvasOpacity] of the [Canvas] in the [DrawBox] */
+    var canvasOpacity: MutableStateFlow<Float> = MutableStateFlow(1f)
+
+    /** An [opacity] of the stroke */
+    var opacity: MutableStateFlow<Float> = MutableStateFlow(1f)
+
+    /** A [strokeWidth] of the stroke */
+    var strokeWidth: MutableStateFlow<Float> = MutableStateFlow(10f)
+
+    /** A [color] of the stroke */
+    var color: MutableStateFlow<Color> = MutableStateFlow(Color.Red)
+
+    /** A [background] of the background of DrawBox */
+    var background: MutableStateFlow<DrawBoxBackground> = MutableStateFlow(DrawBoxBackground.NoBackground)
 }
