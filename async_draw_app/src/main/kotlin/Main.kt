e32e090d55764aa22d5d824722226d1fd9bef89b
Heylium <tuanhsiang@163.com>
2023-09-13 22:51:27 +0800

    Update Main.kt
    App init window size modified.

--- async_draw_app/src/main/kotlin/Main.kt
+++ async_draw_app/src/main/kotlin/Main.kt
@@ -1,11 +1,18 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowState
+
 import androidx.compose.ui.window.application
 import drawbox.canvas.DrawBox2
 
@@ -20,7 +27,12 @@ fun App() {
 }
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
-        App()
+    Window(
+        onCloseRequest = ::exitApplication,
+        state = WindowState(size = DpSize.Unspecified),
+    ) {
+        Box(modifier = Modifier.size(1000.dp, 800.dp)) {
+            App()
+        }
     }
 }
