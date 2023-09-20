829c5e4cf287cb3a56505f0e6d1577d8abe34acc
Heylium <tuanhsiang@163.com>
2023-09-21 00:30:02 +0800

    Update Bond.kt

--- async_draw_app/src/main/kotlin/view/Bond.kt
+++ async_draw_app/src/main/kotlin/view/Bond.kt
@@ -1,8 +1,14 @@
 package view
 
-class Bond: Drawable {
-    override fun draw() {
-        TODO("Not yet implemented")
-    }
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.DrawScope
 
+class Bond(val path: Path, val color: Color): Drawable {
+    override fun draw(drawScope: DrawScope) {
+        drawScope.drawPath(
+            path = path,
+            color = color,
+        )
+    }
 }
