50cfd1f69429669b00d2a9468cf0a0c3958418d7
Heylium <tuanhsiang@163.com>
2023-09-23 00:32:11 +0800

    Update main.ts

--- tauri-app/tauri_vite/src/main.ts
+++ tauri-app/tauri_vite/src/main.ts
@@ -1,8 +1,10 @@
+// @ts-ignore
 import { createApp } from 'vue';
 import './style.css';
 // @ts-ignore
 import App from './App.vue';
 
+// @ts-ignore
 import {createRouter, createWebHashHistory} from "vue-router";
 
 // @ts-ignore
@@ -24,11 +26,10 @@ const router = createRouter({
     routes,
 });
 
+
 // 3. 创建路由实例并传递 `routes` 配置
 // 你可以在这里输入更多的配置，但我们在这里
 // 暂时保持简单
-
-
 createApp(App)
     .use(router)
     .mount('#app')
