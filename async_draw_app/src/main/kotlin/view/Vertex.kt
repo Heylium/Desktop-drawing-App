0a28108835213fc23c11551acb37bf5fab2d4f27
Heylium <tuanhsiang@163.com>
2023-09-24 23:30:56 +0800

    Update Vertex.kt
    Modified Vertex draw() action function to return a lambda function which runs a drawPath() operation.

--- async_draw_app/src/main/kotlin/view/Vertex.kt
+++ async_draw_app/src/main/kotlin/view/Vertex.kt
@@ -9,13 +9,26 @@ import androidx.compose.ui.graphics.drawscope.Stroke
 import io.github.markyav.drawbox.model.PathWrapper
 import io.github.markyav.drawbox.util.createPath
 
-class Vertex(val pathWrapper: PathWrapper, val color: Color = Color.Black): Drawable {
+class Vertex(
+    val pathWrapper: PathWrapper,
+    val width: Float,
+    val color: Color = Color.Black
+) : Drawable {
 
     override fun createDrawAction(): DrawAction {
         val drawObject: Vertex = this;
         return {
             val drawScope: DrawScope = this;
-            
+            drawPath(
+                path = createPath(pathWrapper.points),
+                color = color,
+                style = Stroke(
+                    width = width,
+                    cap = StrokeCap.Round,
+                    join = StrokeJoin.Round,
+                )
+            )
+
         }
     }
 }
