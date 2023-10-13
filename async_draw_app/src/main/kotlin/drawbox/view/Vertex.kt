5d4c8f0bcb63de43fe00e34b42be9afaf1bfb8ee
Heylium <tuanhsiang@163.com>
2023-10-14 00:10:03 +0800

    Create Vertex.kt

---
+++ async_draw_app/src/main/kotlin/drawbox/view/Vertex.kt
@@ -0,0 +1,33 @@
+package drawbox.view
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.StrokeJoin
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.Stroke
+import io.github.markyav.drawbox.model.PathWrapper
+import io.github.markyav.drawbox.util.createPath
+
+class Vertex(
+    val pathWrapper: PathWrapper,
+    val width: Float,
+    val color: Color = Color.Black
+) : Drawable {
+
+    override fun createDrawAction(): DrawAction {
+        val drawObject: Vertex = this
+        return {
+            val drawScope: DrawScope = this
+            drawPath(
+                path = createPath(pathWrapper.points),
+                color = color,
+                style = Stroke(
+                    width = width,
+                    cap = StrokeCap.Round,
+                    join = StrokeJoin.Round,
+                )
+            )
+        }
+    }
+}
