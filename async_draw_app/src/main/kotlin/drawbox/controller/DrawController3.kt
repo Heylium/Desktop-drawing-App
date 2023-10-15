9614766f3e4cbe67b1d210e834908e9724c0d206
Heylium <tuanhsiang@163.com>
2023-10-16 00:03:25 +0800

    Update DrawController3.kt

--- async_draw_app/src/main/kotlin/drawbox/controller/DrawController3.kt
+++ async_draw_app/src/main/kotlin/drawbox/controller/DrawController3.kt
@@ -1,14 +1,26 @@
 package drawbox.controller
 
 import drawbox.view.DrawAction
+import io.github.markyav.drawbox.controller.DrawBoxSubscription
+import io.github.markyav.drawbox.util.combineStates
 import kotlinx.coroutines.flow.*
 
 class DrawController3 {
 
     /**
-     * canvas [DrawAction] State list
+     * canvas [DrawAction] State list.
      */
     private val drawActions: MutableStateFlow<List<DrawAction>> = MutableStateFlow(emptyList())
 
+    private fun _getDynamicUpdateDrawAction(): StateFlow<List<DrawAction>> {
+//        return combineStates(dr)
+    }
+
+    internal fun getDrawAction(subscription: DrawBoxSubscription): StateFlow<List<DrawAction>> {
+        return when (subscription) {
+            is DrawBoxSubscription.DynamicUpdate -> _getDynamicUpdateDrawAction()
+            is DrawBoxSubscription.FinishDrawingUpdate -> drawActions
+        }
+    }
 
 }
