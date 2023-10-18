f6d735da448009987f58b3a6951565f3403e78f3
Heylium <tuanhsiang@163.com>
2023-10-18 22:12:15 +0800

    Vue component modified.

--- tauri-app/tauri_vite/src/App.vue
+++ tauri-app/tauri_vite/src/App.vue
@@ -11,30 +11,7 @@ import Editor from "./components/Editor.vue";
     <router-link to="/editor">To Editor</router-link>
   </p>
   <router-view></router-view>
-<!--  <div>-->
-<!--    <a href="https://vitejs.dev" target="_blank">-->
-<!--      <img src="/vite.svg" class="logo" alt="Vite logo" />-->
-<!--    </a>-->
-<!--    <a href="https://vuejs.org/" target="_blank">-->
-<!--      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />-->
-<!--    </a>-->
-<!--  </div>-->
-<!--  <Editor/>-->
-<!--  <hello-world></hello-world>-->
-
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
--- tauri-app/tauri_vite/src/main.ts
+++ tauri-app/tauri_vite/src/main.ts
@@ -15,7 +15,6 @@ import Editor from "./components/Editor.vue";
 
 // 2. 定义一些路由
 // 每个路由都需要映射到一个组件。
-// 我们后面再讨论嵌套路由。
 const routes = [
     {path: '/', component: HelloWorld},
     {path: '/editor', component: Editor},
