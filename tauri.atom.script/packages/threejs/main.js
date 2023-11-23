c478fd57c4473741106870b5adb5cc837546fd0b
Heylium <tuanhsiang@163.com>
2023-11-23 23:24:25 +0800

    first render code added.

--- tauri.atom.script/packages/threejs/main.js
+++ tauri.atom.script/packages/threejs/main.js
@@ -2,17 +2,37 @@ import * as THREE from "three"
 
 const scene = new THREE.Scene();
 
-const geometry = new THREE.BoxGeometry(100, 100, 100);
+const camera = new THREE.PerspectiveCamera();
+camera.position.z = 10;
+camera.position.y = 2;
 
+const geometry = new THREE.BoxGeometry(2, 2, 2);
+
+// scene material
 const material = new THREE.MeshBasicMaterial({
-    color: 0xff0000,
-})
+    color: 0x00ff00,
+});
 
+// scene mesh
 const mesh = new THREE.Mesh(geometry, material);
-
-mesh.position.set(0,0,0)
-
+mesh.position.set(0, 3)
 scene.add(mesh);
 
 console.log(mesh);
 
+// using webgl2 renderer
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement);
+
+const grid = new THREE.GridHelper(10, 10)
+scene.add(grid)
+
+
+// add animation with mousemove
+const animate = () => {
+    requestAnimationFrame(animate)
+    mesh.rotation.x += 0.01;
+    renderer.render(scene, camera);
+}
+animate();
