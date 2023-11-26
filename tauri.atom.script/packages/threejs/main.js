520fa45f03d02b600da6bb9ffecc8c78bac30819
Heylium <tuanhsiang@163.com>
2023-11-27 00:21:01 +0800

    OrbitControl added.

--- tauri.atom.script/packages/threejs/main.js
+++ tauri.atom.script/packages/threejs/main.js
@@ -1,4 +1,5 @@
 import * as THREE from "three"
+import {OrbitControls} from "three/addons";
 
 const scene = new THREE.Scene();
 
@@ -15,7 +16,7 @@ const material = new THREE.MeshBasicMaterial({
 
 // scene mesh
 const mesh = new THREE.Mesh(geometry, material);
-mesh.position.set(0, 3)
+mesh.position.set(0, 3, 0)
 scene.add(mesh);
 
 console.log(mesh);
@@ -25,14 +26,20 @@ const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement);
 
+const control = new OrbitControls(camera, renderer.domElement);
+
 const grid = new THREE.GridHelper(10, 10)
 scene.add(grid)
 
 
-// add animation with mousemove
+// add animation
 const animate = () => {
     requestAnimationFrame(animate)
     mesh.rotation.x += 0.01;
+
+    // update controller
+    control.update()
+
     renderer.render(scene, camera);
 }
 animate();
