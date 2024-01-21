3c77e5d12632b6dd6941789669deb71030502e54
Heylium <tuanhsiang@163.com>
2024-01-21 23:54:18 +0800

    Update main.ts

--- tauri.atom.script/apps/vite.threejs/src/main.ts
+++ tauri.atom.script/apps/vite.threejs/src/main.ts
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import './style.css'
+import routers from "./routers";
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+const app = createApp(App);
+app
+    .use(routers)
+    .mount('#app')
+
