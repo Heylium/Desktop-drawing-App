4de1944d9c1def0cb159f05ff0089c4ae63397d8
Heylium <tuanhsiang@163.com>
2024-03-27 00:21:17 +0800

    Create Button.vue

---
+++ tauri.atom.script/apps/vite_components/src/components/Button/Button.vue
@@ -0,0 +1,35 @@
+<script lang="ts">
+import {defineComponent} from "vue";
+export default defineComponent({
+  name: 'my-button'
+})
+</script>
+<script setup lang="ts">
+import {ButtonProps} from './types.ts'
+
+defineProps<ButtonProps>()
+</script>
+
+<template>
+  <button
+      class="my-button"
+      :class="{
+        [`my-button--${type}`]: type,
+        [`my-button--${size}`]: size,
+        'is-plain': plain,
+        'is-round': round,
+        'is-circle': circle,
+        'is-disabled': disabled,
+      }"
+      :disabled="disabled"
+  >
+    <span>
+      <slot></slot>
+    </span>
+  </button>
+
+</template>
+
+<style scoped>
+
+</style>
