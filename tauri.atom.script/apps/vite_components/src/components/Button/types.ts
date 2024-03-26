8dc82d10aab191991f0888c53eb43e070719bca7
Heylium <tuanhsiang@163.com>
2024-03-27 00:21:29 +0800

    Update types.ts

--- tauri.atom.script/apps/vite_components/src/components/Button/types.ts
+++ tauri.atom.script/apps/vite_components/src/components/Button/types.ts
@@ -1,5 +1,6 @@
 export type ButtonType = 'primary' | 'success' | 'warning' | 'danger' | 'info'
 export type ButtonSize = 'large' | 'small'
+export type NativeType = 'button' | 'submit' | 'reset'
 
 export interface ButtonProps {
     type?: ButtonType;
@@ -8,4 +9,6 @@ export interface ButtonProps {
     round?: boolean;
     circle?: boolean;
     disabled?: boolean;
+    nativeType?: NativeType;
+    autofocus?: boolean;
 }
