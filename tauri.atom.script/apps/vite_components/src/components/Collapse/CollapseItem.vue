2356d81bde6a6cd178bed80a9026b1841e92ea3d
Heylium <tuanhsiang@163.com>
2024-04-04 01:04:51 +0800

    Create CollapseItem.vue

---
+++ tauri.atom.script/apps/vite_components/src/components/Collapse/CollapseItem.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import {CollapseItemProps} from "./types.ts";
+
+defineOptions({
+  name: 'VkCollapseItem',
+})
+defineProps<CollapseItemProps>();
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
