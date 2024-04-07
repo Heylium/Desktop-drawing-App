d4869820b6ab709dd005e8e3441bd979e0328122
Heylium <tuanhsiang@163.com>
2024-04-08 00:18:08 +0800

    Update CollapseItem.vue

--- tauri.atom.script/apps/vite_components/src/components/Collapse/CollapseItem.vue
+++ tauri.atom.script/apps/vite_components/src/components/Collapse/CollapseItem.vue
@@ -9,7 +9,20 @@ defineProps<CollapseItemProps>();
 </script>
 
 <template>
+<div
+  class="vk-collapse-item"
+  :class="{
+      'is-disabled': disabled
+    }"
+>
+  <div class="vk-collapse-item__header" :id="`item-header-${name}`">
+    <slot name="title">{{title}}</slot>
+  </div>
 
+  <div class="vk-collapse-item__content" :id="`item-content-${name}`">
+    <slot></slot>
+  </div>
+</div>
 </template>
 
 <style scoped>
