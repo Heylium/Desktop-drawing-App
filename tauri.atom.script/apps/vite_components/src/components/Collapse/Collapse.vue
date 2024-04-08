e2fcab2a0d279ac0b77f06499b46f630fdddfb6b
Heylium <tuanhsiang@163.com>
2024-04-09 00:11:17 +0800

    Update Collapse.vue

--- tauri.atom.script/apps/vite_components/src/components/Collapse/Collapse.vue
+++ tauri.atom.script/apps/vite_components/src/components/Collapse/Collapse.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
-import {ref} from "vue";
+import {ref, provide} from "vue"
 import type {NameType} from "./types.ts";
+import {collapseContextKey} from "./types.ts";
 
 
 defineOptions({
   name: 'VkCollapse'
 })
 const activeNames = ref<NameType[]>([])
-const handleItenClick = (item: NameType) => {
+const handleItemClick = (item: NameType) => {
   const index = activeNames.value.indexOf(item)
   if (index > -1) {
     activeNames.value.splice(index, 1)
@@ -15,6 +16,11 @@ const handleItenClick = (item: NameType) => {
     activeNames.value.push(item)
   }
 }
+
+provide(collapseContextKey, {
+  activeNames,
+  handleItemClick,
+})
 </script>
 
 <template>
