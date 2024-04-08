2d13d3f80eba862c276966631d6137588a125184
Heylium <tuanhsiang@163.com>
2024-04-09 00:10:56 +0800

    Update types.ts

--- tauri.atom.script/apps/vite_components/src/components/Collapse/types.ts
+++ tauri.atom.script/apps/vite_components/src/components/Collapse/types.ts
@@ -1,4 +1,7 @@
 
+import type {Ref, InjectionKey} from 'vue'
+
+
 export type NameType = string | number
 
 export interface CollapseItemProps {
@@ -6,3 +9,10 @@ export interface CollapseItemProps {
     title?: string;
     disabled?: boolean;
 }
+
+export interface CollapseContext {
+    activeNames: Ref<NameType[]>;
+    handleItemClick: (name: NameType) => void;
+}
+
+export const collapseContextKey: InjectionKey<CollapseContext> = Symbol('collapseContext')
