8a9f944d2c94585d88108323a9532369b9f3607a
Heylium <tuanhsiang@163.com>
2024-04-12 00:08:31 +0800

    CollapseItem fade events added.

--- tauri.atom.script/apps/vite_components/src/components/Collapse/CollapseItem.vue
+++ tauri.atom.script/apps/vite_components/src/components/Collapse/CollapseItem.vue
@@ -15,6 +15,26 @@ const handleClick = () => {
   if (props.disabled) return
   collapseContext?.handleItemClick(props.name)
 }
+const transitionEvents: Record<string, (el: HTMLElement) => void> = {
+  beforeEnter(el) {
+    el.style.height = '0px'
+  },
+  enter(el) {
+    el.style.height = `${el.scrollHeight}px`
+  },
+  afterEnter(el) {
+    el.style.height = ``
+  },
+  beforeLeave(el) {
+    el.style.height = `${el.scrollHeight}px`
+  },
+  leave(el) {
+    el.style.height = '0px'
+  },
+  afterLeave(el) {
+    el.style.height = ''
+  }
+}
 </script>
 
 <template>
@@ -28,7 +48,7 @@ const handleClick = () => {
   <div class="vk-collapse-item__header" :id="`item-header-${name}`" @click="handleClick">
     <slot name="title">{{title}}</slot>
   </div>
-  <Transition name="fade">
+  <Transition name="fade" v-on="transitionEvents">
     <div class="vk-collapse-item__content" :id="`item-content-${name}`" v-show="isActive">
       <slot></slot>
     </div>
