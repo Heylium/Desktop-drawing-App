faf6c09472f57f886aca2b374b9dd0ed9a23afe7
Heylium <tuanhsiang@163.com>
2024-04-16 00:28:44 +0800

    fontAwesomeIcon component added.

--- tauri.atom.script/apps/vite_components/src/main.ts
+++ tauri.atom.script/apps/vite_components/src/main.ts
@@ -1,9 +1,15 @@
 import { createApp } from 'vue'
+import {library} from "@fortawesome/fontawesome-svg-core";
+import {faUserSecret} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
 import App from './App.vue'
 
+library.add(faUserSecret)
 // import './style.css'
 import './styles/index.css'
 
 
 
-createApp(App).mount('#app')
+createApp(App)
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .mount('#app')
