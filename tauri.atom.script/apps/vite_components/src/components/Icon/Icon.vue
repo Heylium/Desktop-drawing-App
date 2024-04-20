f3177d58872ed7de6e8ce7a38b44826d0a348ad8
Heylium <tuanhsiang@163.com>
2024-04-21 00:45:51 +0800

    Icon component created.

---
+++ tauri.atom.script/apps/vite_components/src/components/Icon/Icon.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+// import type { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import {IconProps} from "./types.ts";
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+
+
+defineOptions({
+  name: 'VkIcon',
+  inheritAttrs: false,
+})
+
+defineProps<IconProps>()
+
+</script>
+
+<template>
+  <i class="vk-icon">
+    <font-awesome-icon v-bind="$props"/>
+  </i>
+
+</template>
+
+<style scoped>
+
+</style>
