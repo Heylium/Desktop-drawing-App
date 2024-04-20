d168293323852f44cfb061ccadfec172ea7bffc9
Heylium <tuanhsiang@163.com>
2024-04-21 00:45:31 +0800

    Update main.ts

--- tauri.atom.script/apps/vite_components/src/main.ts
+++ tauri.atom.script/apps/vite_components/src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import {library} from "@fortawesome/fontawesome-svg-core";
 import {faUserSecret} from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
 import App from './App.vue'
 
 library.add(faUserSecret)
@@ -11,5 +10,4 @@ import './styles/index.css'
 
 
 createApp(App)
-    .component('font-awesome-icon', FontAwesomeIcon)
     .mount('#app')
