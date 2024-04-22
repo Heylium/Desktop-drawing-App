da9745cf0f9967e8088e2018e6cb42c80a93cf8f
Heylium <tuanhsiang@163.com>
2024-04-22 23:58:47 +0800

    added fontawesome icons library

--- tauri.atom.script/apps/vite_components/src/main.ts
+++ tauri.atom.script/apps/vite_components/src/main.ts
@@ -1,9 +1,9 @@
 import { createApp } from 'vue'
-import {library} from "@fortawesome/fontawesome-svg-core";
-import {faUserSecret} from "@fortawesome/free-solid-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {fas} from "@fortawesome/free-solid-svg-icons"
 import App from './App.vue'
 
-library.add(faUserSecret)
+library.add(fas)
 // import './style.css'
 import './styles/index.css'
 
