b081f66df7020ab6f8dc491135636f03e5204943
Heylium <tuanhsiang@163.com>
2024-04-25 00:18:58 +0800

    Vitest framework added.

---
+++ tauri.atom.script/apps/vite_components/example.test.ts
@@ -0,0 +1,10 @@
+import { expect, test} from "vitest";
+
+
+test('test common matcher', () => {
+    const name = 'mk'
+    expect(name).toBe('mk')
+
+    expect(2 + 2).toBe(4)
+    expect(2 + 2).not.toBe(5)
+})
