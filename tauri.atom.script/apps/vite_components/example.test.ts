b386b8bb472612e7b08397dfaa6fff10c9b44488
Heylium <tuanhsiang@163.com>
2024-04-26 00:39:44 +0800

    test third party lib added.

--- tauri.atom.script/apps/vite_components/example.test.ts
+++ tauri.atom.script/apps/vite_components/example.test.ts
@@ -1,5 +1,10 @@
-import {describe, expect, test, vi} from "vitest";
-import {testFn} from "./utils";
+import {describe, expect, test, vi, Mocked} from "vitest";
+import {testFn, request} from "./utils";
+import axios from "axios";
+
+
+vi.mock('axios')
+const mockedAxios = axios as Mocked<typeof axios>
 
 test('test common matcher', () => {
     const name = 'mk'
@@ -32,6 +37,9 @@ describe('functions', () => {
         expect(spy).toHaveBeenCalledTimes(2)
     })
 
-    
-
+    test('mock third party module', async () => {
+        mockedAxios.get.mockImplementation(() => Promise.resolve({data: 123}))
+        const result = await request()
+        expect(result).toBe(123)
+    })
 })
