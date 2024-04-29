f562311d3040a4cf35c784ef88514b40bd2e31a6
Heylium <tuanhsiang@163.com>
2024-04-29 23:04:51 +0800

    button component tests added.

--- tauri.atom.script/apps/vite_components/src/components/Button/Button.test.ts
+++ tauri.atom.script/apps/vite_components/src/components/Button/Button.test.ts
@@ -1,4 +1,4 @@
-import { describe, test } from "vitest";
+import { describe, test, expect } from "vitest";
 import { mount } from "@vue/test-utils";
 import Button from "./Button.vue";
 
@@ -14,5 +14,36 @@ describe('Button.vue', () => {
             }
         })
         console.log(wrapper.html())
+        expect(wrapper.classes()).toContain('vk-button--primary')
+
+        // test slot
+        expect(wrapper.get('button').text()).toBe('button')
+
+        // events
+        wrapper.get('button').trigger('click')
+        console.log(wrapper.emitted())
+        expect(wrapper.emitted()).toHaveProperty('click')
+    })
+
+    test('disabled', () => {
+        const wrapper = mount(Button, {
+            props: {
+                disabled: true,
+            },
+            slots: {
+                default: 'disabled',
+            }
+        })
+
+        // button attribute
+        expect(wrapper.attributes('disabled')).toBeDefined()
+
+        // button html node
+        expect(wrapper.find('button').element.disabled).toBeDefined()
+
+        // click disabled button
+        wrapper.get('button').trigger('click')
+        console.log(wrapper.emitted())
+        expect(wrapper.emitted()).not.toHaveProperty('click')
     })
 })
