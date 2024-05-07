7d6e4029de2cda6ab808cbc5415853493d1f95c7
Heylium <tuanhsiang@163.com>
2024-05-08 00:03:00 +0800

    Button component tests added.

--- tauri.atom.script/apps/vite_components/src/components/Button/Button.test.ts
+++ tauri.atom.script/apps/vite_components/src/components/Button/Button.test.ts
@@ -1,6 +1,8 @@
 import { describe, test, expect } from "vitest";
 import { mount } from "@vue/test-utils";
 import Button from "./Button.vue";
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+import VkIcon from "../Icon/Icon.vue";
 
 
 describe('Button.vue', () => {
@@ -45,5 +47,41 @@ describe('Button.vue', () => {
         wrapper.get('button').trigger('click')
         console.log(wrapper.emitted())
         expect(wrapper.emitted()).not.toHaveProperty('click')
+    })
+
+    test('icon', () => {
+        const wrapper = mount(Button, {
+            props: {
+                icon: 'arrow-up'
+            },
+            slots: {
+                default: 'icon'
+            },
+            global: {
+                stubs: ['FontAwesomeIcon']
+            }
+        })
+        console.log(wrapper.html())
+        const iconElement = wrapper.findComponent(FontAwesomeIcon)
+        expect(iconElement.exists()).toBeTruthy()
+        expect(iconElement.attributes('icon')).toBe('arrow-up')
+    })
+
+    test('loading', () => {
+        const wrapper = mount(Button, {
+            props: {
+                loading: true,
+            },
+            slots: {
+                default: 'loading'
+            },
+            global: {
+                stubs: ['Icon']
+            }
+        })
+        const iconElement = wrapper.findComponent(VkIcon)
+        expect(iconElement.exists()).toBeTruthy()
+        expect(iconElement.attributes('icon')).toBe('spinner')
+        expect(wrapper.attributes('disabled')).toBeDefined()
     })
 })
