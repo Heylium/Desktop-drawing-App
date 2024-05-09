d1a8fbc4d247fd1c0711dd5afcefdd84dd48d167
Heylium <tuanhsiang@163.com>
2024-05-10 00:36:27 +0800

    Create VNode.vue

---
+++ tauri.atom.script/apps/vite_components/src/VNode.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+import {h, defineComponent, ref} from "vue";
+
+export default defineComponent({
+  name: 'VNode',
+  props: {
+    msg: {
+      type: String,
+      required: true,
+    }
+  },
+  setup(props) {
+    const count = ref(1)
+    return () => h('h1', props.msg + count.value)
+  }
+})
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
