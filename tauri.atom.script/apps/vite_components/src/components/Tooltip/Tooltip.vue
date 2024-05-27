46175be974e446ce13a4499b84cb7563849f8e3f
Heylium <tuanhsiang@163.com>
2024-05-28 00:53:10 +0800

    Update Tooltip.vue

--- tauri.atom.script/apps/vite_components/src/components/Tooltip/Tooltip.vue
+++ tauri.atom.script/apps/vite_components/src/components/Tooltip/Tooltip.vue
@@ -1,4 +1,32 @@
 <script setup lang="ts">
+import {ref, watch} from "vue";
+import type {TooltipProps, TooltipEmits} from "./types.ts";
+import {createPopper, Instance} from "@popperjs/core";
+
+
+const props = withDefaults(defineProps<TooltipProps>(), {
+  placement: 'bottom',
+})
+const emits = defineEmits<TooltipEmits>()
+const isOpen = ref(false)
+const popperNode = ref<HTMLElement>()
+const triggerNode = ref<HTMLElement>()
+let popperInstance: null | Instance = null
+const togglePopper = () => {
+  isOpen.value = !isOpen.value
+  emits('visible-change', isOpen.value)
+}
+watch(isOpen, (newValue) => {
+  if (newValue) {
+    if (triggerNode.value && popperNode.value) {
+      popperInstance = createPopper(triggerNode.value, popperNode.value, {
+        placement: props.placement,
+      })
+    } else {
+      popperInstance?.destroy()
+    }
+  }
+}, { flush: 'post' })
 
 </script>
 
@@ -7,13 +35,20 @@
   class="vk-tooltip"
 >
   <div
-    class="vk-tooltip__trigger"
+      ref="triggerNode"
+      class="vk-tooltip__trigger"
+      @click="togglePopper"
   >
     <slot></slot>
   </div>
   <div
-    class="vk-tooltip_popper"
+      v-if="isOpen"
+      ref="popperNode"
+      class="vk-tooltip_popper"
   >
+    <slot name="content">
+      {{ content }}
+    </slot>
   </div>
 
 </div>
