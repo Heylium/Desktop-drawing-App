9621ebdb3f159e37b283ad5f1ee4d269b0bb1eb7
Heylium <tuanhsiang@163.com>
2024-07-10 00:39:39 +0800

    Update Tooltip.test.tsx

--- tauri.atom.script/apps/vite_components/src/components/Tooltip/Tooltip.test.tsx
+++ tauri.atom.script/apps/vite_components/src/components/Tooltip/Tooltip.test.tsx
@@ -1,17 +1,24 @@
-import {describe, expect, test, vi} from "vitest";
+import {beforeEach, describe, expect, test, vi} from "vitest";
 import {mount} from "@vue/test-utils";
 import Tooltip from "./Tooltip.vue";
-import Button from "../Button/Button.vue";
 
 
+vi.mock('@popperjs/core')
 const onVisibleChange = vi.fn()
 
 describe('Tooltip.vue', () => {
+  beforeEach(() => {
+    vi.useFakeTimers()
+  })
+
   test('basic tooltip', async () => {
     const wrapper = mount(() =>
-      <Tooltip content="hello tooltip" trigger="click" onVisibleChange={onVisibleChange}>
-        <button id="trigger">Trigger</button>
-      </Tooltip>
+      <div>
+        <div id="outside"></div>
+        <Tooltip content="hello tooltip" trigger="click" onVisibleChange={onVisibleChange}>
+          <button id="trigger">Trigger</button>
+        </Tooltip>
+      </div>
       , {
         attachTo: document.body
       }
@@ -20,6 +27,21 @@ describe('Tooltip.vue', () => {
     // static test
     const triggerArea = wrapper.find('#trigger')
     expect(triggerArea.exists()).toBeTruthy()
+    expect(wrapper.find('.vk-tooltip_popper').exists()).toBeFalsy()
+
+    // testing click
+    triggerArea.trigger('click')
+    await vi.runAllTimers()
+    // error
+    expect(wrapper.find('.vk-tooltip_popper').exists()).toBeTruthy()
+    expect(wrapper.get('.vk-tooltip_popper').text()).toBe('hello tooltip')
+    expect('onVisibleChange').toHaveBeenCalledWith(true)
+    console.log(wrapper.html())
+
+    wrapper.find('#outside').trigger('click')
+    await vi.runAllTimers()
+    expect(wrapper.find('.vk-tooltip_popper').exists()).toBeFalsy()
+    expect('onVisibleChange').toHaveBeenLastCalledWith(true)
 
 
   })
