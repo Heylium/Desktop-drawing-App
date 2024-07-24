b3823235828a5239593da8dc1e14f0c83a4690fe
Heylium <tuanhsiang@163.com>
2024-07-25 00:30:03 +0800

    Create useEventListener.ts

---
+++ tauri.atom.script/apps/vite_components/src/hooks/useEventListener.ts
@@ -0,0 +1,24 @@
+import {isRef, onBeforeUnmount, onMounted, unref, watch} from "vue";
+import type {Ref} from "vue";
+
+
+export default function useEventListeners(
+  target: Ref<EventTarget | null> | EventTarget,
+  event: string,
+  handler: (e: Event) => any,
+) {
+  if (isRef(target)) {
+    watch(target, (value, oldValue) => {
+      oldValue?.removeEventListener(event, handler)
+      value?.addEventListener(event, handler)
+    })
+  } else {
+    onMounted(() => {
+      target.addEventListener(event, handler)
+    })
+  }
+
+  onBeforeUnmount(() => {
+    unref(target)?.removeEventListener(event, handler)
+  })
+}
