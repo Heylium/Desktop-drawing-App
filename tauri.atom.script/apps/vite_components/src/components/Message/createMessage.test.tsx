17fda664e53303eb664a41ac1dbd50581a4a2737
Heylium <tuanhsiang@163.com>
2024-07-26 23:49:52 +0800

    Create createMessage.test.tsx

---
+++ tauri.atom.script/apps/vite_components/src/components/Message/createMessage.test.tsx
@@ -0,0 +1,18 @@
+import {describe, test} from "vitest";
+
+
+describe('createMessage', () => {
+
+  test('调用方法应该创建对应的 Message 组件', async () => {
+
+  })
+
+  test('多次调用方法应该创建多个实例', async () => {
+
+  })
+
+  test('创建多个实例应该设置正确的 offset', async () => {
+
+  })
+
+});
