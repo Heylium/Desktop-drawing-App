ebe4d6914f2da12fa6b636bf2ae35cc79355909d
Heylium <tuanhsiang@163.com>
2024-07-27 20:42:11 +0800

    vite preview plugin added

--- tauri.atom.script/apps/vite_components/docs/.vitepress/config.ts
+++ tauri.atom.script/apps/vite_components/docs/.vitepress/config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vitepress'
+import { fileURLToPath, URL } from 'node:url'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import VueMacros from "unplugin-vue-macros";
+import VueMacros from 'unplugin-vue-macros'
+import { containerPreview, componentPreview } from '@vitepress-demo-preview/plugin'
 
 
 // https://vitepress.dev/reference/site-config
@@ -13,10 +15,21 @@ export default defineConfig({
         setupComponent: false,
         setupSFC: false,
         plugins: {
-          vuejs: vueJsx(),
-        }
-      })
+          vueJsx: vueJsx(),
+        },
+      }),
     ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('../../src', import.meta.url))
+      }
+    }
+  },
+  markdown: {
+    config(md) {
+      md.use(containerPreview)
+      md.use(componentPreview)
+    }
   },
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
